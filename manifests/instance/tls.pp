1b37243b53149493eaf0d868d83f2d2b8bdc02ce
lnemsick-simp <lnemsick.simp@gmail.com>
2021-06-15 11:13:16 -0400

    Do not do TLS management if ds389::instnace::tls::ensure = false

--- manifests/instance/tls.pp
+++ manifests/instance/tls.pp
@@ -11,7 +11,15 @@
 #   Path of file containing pasword fo rthe default administrator DN
 #
 # @param ensure
-#   Ensure of PKI resources managed by this define
+#   How PKI resources are to be managed by this define
+#
+#   'simp'     => enables TLS and copies the certs from the puppetserver
+#                 using the SIMP pki module.
+#   'true'     => enables TLS and copies the certs from a location on the
+#                 local system. See pki module to see the required
+#                 configuration of the directory.
+#   'false'    => Do nothing with the TLS settings and certs.
+#   'disabled' => Disable TLS on the instance.
 #
 # @param port
 #   The port upon which to accept LDAPS connections
@@ -57,149 +65,151 @@ define ds389::instance::tls (
 ) {
   assert_private()
 
-  $_default_dse_config = {
-    'cn=encryption,cn=config' => {
-      'allowWeakCipher'               => 'off',
-      'allowWeakDHParam'              => 'off',
-      'nsSSL2'                        => 'off',
-      'nsSSL3'                        => 'off',
-      'nsSSLClientAuth'               => 'required',
-      'nsTLS1'                        => 'on',
-      'nsTLSAllowClientRenegotiation' => 'on',
-      'sslVersionMax'                 => 'TLS1.2',
-      'sslVersionMin'                 => 'TLS1.2'
-    },
-    'cn=config'               => {
-      'nsslapd-ssl-check-hostname' => 'on',
-      'nsslapd-validate-cert'      => 'on',
-      'nsslapd-minssf'             => 256
+  if $ensure {
+    $_default_dse_config = {
+      'cn=encryption,cn=config' => {
+        'allowWeakCipher'               => 'off',
+        'allowWeakDHParam'              => 'off',
+        'nsSSL2'                        => 'off',
+        'nsSSL3'                        => 'off',
+        'nsSSLClientAuth'               => 'required',
+        'nsTLS1'                        => 'on',
+        'nsTLSAllowClientRenegotiation' => 'on',
+        'sslVersionMax'                 => 'TLS1.2',
+        'sslVersionMin'                 => 'TLS1.2'
+      },
+      'cn=config'               => {
+        'nsslapd-ssl-check-hostname' => 'on',
+        'nsslapd-validate-cert'      => 'on',
+        'nsslapd-minssf'             => 256
+      }
     }
-  }
 
-  $_required_dse_config = {
-    'cn=config' => {
-      'nsslapd-security'   => 'on',
-      'nsslapd-securePort' => $port
+    $_required_dse_config = {
+      'cn=config' => {
+        'nsslapd-security'   => 'on',
+        'nsslapd-securePort' => $port
+      }
     }
-  }
 
-  $_selinux_port_ensure = $ensure ? { false => false, default => true }
-  ds389::instance::selinux::port { String($port):
-    enable  => $_selinux_port_ensure,
-    default => 636,
-    # Leave this commented out until the LDAPI attribute can be moved out into
-    # its own defined typed and called repeatedly.
-    #
-    #instance => $title,
-  }
-
-  if $ensure == 'disabled' {
-    ds389::instance::attr::set { "Do not require encryption for ${title}":
-      instance_name => $title,
-      root_dn       => $root_dn,
-      root_pw_file  => $root_pw_file,
-      force_ldapi   => true,
-      key           => 'nsslapd-minssf',
-      value         => '0'
-    }
-  }
-  else {
     # Check to make sure we're not going to have a conflict with something that's running
     pick($facts['ds389__instances'], {}).each |$daemon, $data| {
-      unless $daemon == $title {
+     unless $daemon == $title {
         if ($data['port'] == $port) or ($data['securePort'] == $port) {
           fail("The port '${port}' is already in use by '${daemon}'")
         }
       }
     }
 
-    if defined_with_params(Ds389::Instance::Tls, { 'ensure' => $ensure, 'port' => $port }) {
-      fail("The port '${port}' is already selected for use by another defined catalog resource")
+    $_selinux_port_ensure = $ensure ? { 'disabled' => false, default => true }
+    ds389::instance::selinux::port { String($port):
+      enable  => $_selinux_port_ensure,
+      default => 636,
+      # Leave this commented out until the LDAPI attribute can be moved out into
+      # its own defined typed and called repeatedly.
+      #
+      #instance => $title,
     }
 
-    # Needed to allow unattended starts
-    # See the 389-DS docs for additional details.
-    $_pin_file = "/etc/dirsrv/slapd-${title}/pin.txt"
-    $_token_file = "/etc/dirsrv/slapd-${title}/p12token.txt"
-
-    file { $_pin_file:
-      group   => $service_group,
-      mode    => '0600',
-      content => Sensitive("Internal (Software) Token:${token}\n")
-    }
-
-    file { $_token_file:
-      mode    => '0400',
-      content => Sensitive($token)
-    }
-
-    if $ensure {
-      simplib::assert_optional_dependency($module_name, 'simp/pki')
-
-      pki::copy { "${module_name}_${title}":
-        source => $source,
-        pki    => $ensure,
-        group  => 'root',
-        notify => Exec["Build ${title} p12"]
+    if $ensure == 'disabled' {
+      ds389::instance::attr::set { "Do not require encryption for ${title}":
+        instance_name => $title,
+        root_dn       => $root_dn,
+        root_pw_file  => $root_pw_file,
+        force_ldapi   => true,
+        key           => 'nsslapd-minssf',
+        value         => '0'
       }
     }
+    else {
+      if defined_with_params(Ds389::Instance::Tls, { 'ensure' => $ensure, 'port' => $port }) {
+        fail("The port '${port}' is already selected for use by another defined catalog resource")
+      }
 
-    $_instance_base = "/etc/dirsrv/slapd-${title}"
-    $_p12_file = "${_instance_base}/puppet_import.p12"
+      # Needed to allow unattended starts
+      # See the 389-DS docs for additional details.
+      $_pin_file = "/etc/dirsrv/slapd-${title}/pin.txt"
+      $_token_file = "/etc/dirsrv/slapd-${title}/p12token.txt"
 
-    exec { "Validate ${title} p12":
-      command => "rm -f ${_p12_file}",
-      unless  => "openssl pkcs12 -nokeys -in ${_p12_file} -passin file:${_token_file}",
-      path    => ['/bin', '/usr/bin'],
-      notify  => Exec["Build ${title} p12"]
-    }
+      file { $_pin_file:
+        group   => $service_group,
+        mode    => '0600',
+        content => Sensitive("Internal (Software) Token:${token}\n")
+      }
 
-    exec { "Build ${title} p12":
-      command     => "openssl pkcs12 -export -name 'Server-Cert' -out ${_p12_file} -in ${key} -certfile ${cert} -passout file:${_token_file}",
-      refreshonly => true,
-      path        => ['/bin', '/usr/bin'],
-      subscribe   => File[$_token_file]
-    }
+      file { $_token_file:
+        mode    => '0400',
+        content => Sensitive($token)
+      }
 
-    exec { "Import ${title} p12":
-      command   => "certutil -D -d ${_instance_base} -n 'Server-Cert' ||:; pk12util -i ${_p12_file} -d ${_instance_base} -w ${_token_file} -k ${_token_file} -n 'Server-Cert'",
-      unless    => "certutil -d ${_instance_base} -L -n 'Server-Cert'",
-      path      => ['/bin', '/usr/bin'],
-      subscribe => Exec["Build ${title} p12"]
-    }
+      if $ensure {
+        simplib::assert_optional_dependency($module_name, 'simp/pki')
 
-    exec { "Import ${title} CA":
-      command   => "certutil -D -d ${_instance_base} -n 'CA Certificate' ||:; certutil -A -i ${cafile} -d ${_instance_base} -n 'CA Certificate' -t 'CT,,' -a -f ${_token_file}",
-      unless    => "certutil -d ${_instance_base} -L -n 'CA Certificate'",
-      path      => ['/bin', '/usr/bin'],
-      subscribe => Exec["Build ${title} p12"]
-    }
+        pki::copy { "${module_name}_${title}":
+          source => $source,
+          pki    => $ensure,
+          group  => 'root',
+          notify => Exec["Build ${title} p12"]
+        }
+      }
 
-    ds389::instance::dn::add { "RSA DN for ${title}":
-      instance_name => $title,
-      dn            => 'cn=RSA,cn=encryption,cn=config',
-      objectclass   => [
-        'top',
-        'nsEncryptionModule'
-      ],
-      attrs         => {
-        'nsSSLPersonalitySSL' => 'Server-Cert',
-        'nsSSLActivation'     => 'on',
-        'nsSSLToken'          => 'internal (software)'
-      },
-      root_dn       => $root_dn,
-      root_pw_file  => $root_pw_file,
-      force_ldapi   => true
-    }
+      $_instance_base = "/etc/dirsrv/slapd-${title}"
+      $_p12_file = "${_instance_base}/puppet_import.p12"
 
-    ds389::instance::attr::set { "Configure PKI for ${title}":
-      instance_name    => $title,
-      root_dn          => $root_dn,
-      root_pw_file     => $root_pw_file,
-      attrs            => $_default_dse_config.deep_merge($dse_config).deep_merge($_required_dse_config),
-      force_ldapi      => true,
-      restart_instance => true,
-      require          => Ds389::Instance::Dn::Add["RSA DN for ${title}"]
+      exec { "Validate ${title} p12":
+        command => "rm -f ${_p12_file}",
+        unless  => "openssl pkcs12 -nokeys -in ${_p12_file} -passin file:${_token_file}",
+        path    => ['/bin', '/usr/bin'],
+        notify  => Exec["Build ${title} p12"]
+      }
+
+      exec { "Build ${title} p12":
+        command     => "openssl pkcs12 -export -name 'Server-Cert' -out ${_p12_file} -in ${key} -certfile ${cert} -passout file:${_token_file}",
+        refreshonly => true,
+        path        => ['/bin', '/usr/bin'],
+        subscribe   => File[$_token_file]
+      }
+
+      exec { "Import ${title} p12":
+        command   => "certutil -D -d ${_instance_base} -n 'Server-Cert' ||:; pk12util -i ${_p12_file} -d ${_instance_base} -w ${_token_file} -k ${_token_file} -n 'Server-Cert'",
+        unless    => "certutil -d ${_instance_base} -L -n 'Server-Cert'",
+        path      => ['/bin', '/usr/bin'],
+        subscribe => Exec["Build ${title} p12"]
+      }
+
+      exec { "Import ${title} CA":
+        command   => "certutil -D -d ${_instance_base} -n 'CA Certificate' ||:; certutil -A -i ${cafile} -d ${_instance_base} -n 'CA Certificate' -t 'CT,,' -a -f ${_token_file}",
+        unless    => "certutil -d ${_instance_base} -L -n 'CA Certificate'",
+        path      => ['/bin', '/usr/bin'],
+        subscribe => Exec["Build ${title} p12"]
+      }
+
+      ds389::instance::dn::add { "RSA DN for ${title}":
+        instance_name => $title,
+        dn            => 'cn=RSA,cn=encryption,cn=config',
+        objectclass   => [
+          'top',
+          'nsEncryptionModule'
+        ],
+        attrs         => {
+          'nsSSLPersonalitySSL' => 'Server-Cert',
+          'nsSSLActivation'     => 'on',
+          'nsSSLToken'          => 'internal (software)'
+        },
+        root_dn       => $root_dn,
+        root_pw_file  => $root_pw_file,
+        force_ldapi   => true
+      }
+
+      ds389::instance::attr::set { "Configure PKI for ${title}":
+        instance_name    => $title,
+        root_dn          => $root_dn,
+        root_pw_file     => $root_pw_file,
+        attrs            => $_default_dse_config.deep_merge($dse_config).deep_merge($_required_dse_config),
+        force_ldapi      => true,
+        restart_instance => true,
+        require          => Ds389::Instance::Dn::Add["RSA DN for ${title}"]
+      }
     }
   }
 }
--- spec/defines/instance/tls_spec.rb
+++ spec/defines/instance/tls_spec.rb
@@ -14,12 +14,20 @@ describe 'ds389::instance::tls', type: :define do
           'test'
         end
 
-        let(:params) do
-          {
-            root_dn: 'dn=thing',
-            root_pw_file: '/some/seekrit/file.skrt',
-            token: '12345678910111213' # For testing
-          }
+        let(:instance_base) do
+            "/etc/dirsrv/slapd-#{title}"
+        end
+
+        let(:p12file) do
+          "#{instance_base}/puppet_import.p12"
+        end
+
+        let(:pin_file) do
+          "#{instance_base}/pin.txt"
+        end
+
+        let(:token_file) do
+          "#{instance_base}/p12token.txt"
         end
 
         let(:pre_condition) do
@@ -30,22 +38,28 @@ describe 'ds389::instance::tls', type: :define do
           PRECOND
         end
 
-        it { is_expected.to compile.with_all_deps }
+        context 'with default parameters' do
+          let(:params) do
+            {
+              root_dn: 'dn=thing',
+              root_pw_file: '/some/seekrit/file.skrt',
+              token: '12345678910111213' # For testing
+            }
+          end
 
-        it do
-          expect(subject).to create_file("/etc/dirsrv/slapd-#{title}/pin.txt")
-            .with_group('dirsrv')
-            .with_mode('0600')
-            .with_content(sensitive("Internal (Software) Token:#{params[:token]}\n"))
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to_not create_ds389__instance__selinux__port('636') }
+          it { is_expected.to_not create_ds389__instance__attr__set("Do not require encryption for #{title}") }
+          it { is_expected.to_not create_file(pin_file) }
+          it { is_expected.to_not create_file(token_file) }
+          it { is_expected.to_not create_pki__copy("ds389_#{title}") }
+          it { is_expected.to_not create_exec("Validate #{title} p12") }
+          it { is_expected.to_not create_exec("Build #{title} p12") }
+          it { is_expected.to_not create_exec("Import #{title} p12") }
+          it { is_expected.to_not create_exec("Import #{title} CA") }
+          it { is_expected.to_not create_ds389__instance__dn__add("RSA DN for #{title}") }
+          it { is_expected.to_not create_ds389__instance__attr__set("Configure PKI for #{title}") }
         end
-
-        it do
-          expect(subject).to create_file("/etc/dirsrv/slapd-#{title}/p12token.txt")
-            .with_mode('0400')
-            .with_content(sensitive(params[:token]))
-        end
-
-        it { is_expected.not_to create_pki__copy("ds389_#{title}") }
 
         context 'with SIMP PKI' do
           let(:params) do
@@ -60,18 +74,28 @@ describe 'ds389::instance::tls', type: :define do
             }
           end
 
-          let(:instance_base) do
-            "/etc/dirsrv/slapd-#{title}"
-          end
-          let(:p12file) do
-            "#{instance_base}/puppet_import.p12"
-          end
-
-          let(:token_file) do
-            "#{instance_base}/p12token.txt"
-          end
-
           it { is_expected.to compile.with_all_deps }
+
+          it do
+            is_expected.to create_ds389__instance__selinux__port('636')
+              .with_enable(true)
+              .with_default(636)
+          end
+
+          it { is_expected.to_not create_ds389__instance__attr__set("Do not require encryption for #{title}") }
+
+          it do
+            is_expected.to create_file(pin_file)
+            .with_group('dirsrv')
+            .with_mode('0600')
+            .with_content(sensitive("Internal (Software) Token:#{params[:token]}\n"))
+          end
+
+          it do
+            is_expected.to create_file(token_file)
+              .with_mode('0400')
+              .with_content(sensitive(params[:token]))
+          end
 
           it do
             expect(subject).to create_pki__copy("ds389_#{title}")
@@ -150,6 +174,40 @@ describe 'ds389::instance::tls', type: :define do
           end
         end
 
+        context 'with non-SIMP PKI' do
+          let(:params) do
+            {
+              ensure: true,
+              root_dn: 'dn=thing',
+              root_pw_file: '/some/seekrit/file.skrt',
+              key: '/my/key',
+              cert: '/my/cert',
+              cafile: '/my/cafile',
+              token: '12345678910111213' # For testing
+            }
+          end
+
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_ds389__instance__selinux__port('636').with_enable(true) }
+          it { is_expected.to create_file(pin_file) }
+          it { is_expected.to create_file(token_file) }
+
+          it do
+            expect(subject).to create_pki__copy("ds389_#{title}")
+              .with_source(%r{/etc/pki})
+              .with_pki(params[:ensure])
+              .with_group('root')
+              .that_notifies("Exec[Build #{title} p12]")
+          end
+
+          it { is_expected.to create_exec("Validate #{title} p12") }
+          it { is_expected.to create_exec("Build #{title} p12") }
+          it { is_expected.to create_exec("Import #{title} p12") }
+          it { is_expected.to create_exec("Import #{title} CA") }
+          it { is_expected.to create_ds389__instance__dn__add("RSA DN for #{title}") }
+          it { is_expected.to create_ds389__instance__attr__set("Configure PKI for #{title}") }
+        end
+
         context 'with PKI disabled' do
           let(:params) do
             {
@@ -157,6 +215,12 @@ describe 'ds389::instance::tls', type: :define do
               root_pw_file: '/some/seekrit/file.skrt',
               :ensure => 'disabled'
             }
+          end
+
+          it do
+            is_expected.to create_ds389__instance__selinux__port('636')
+              .with_enable(false)
+              .with_default(636)
           end
 
           it do
@@ -168,6 +232,16 @@ describe 'ds389::instance::tls', type: :define do
               .with_key('nsslapd-minssf')
               .with_value('0')
           end
+
+          it { is_expected.to_not create_file(pin_file) }
+          it { is_expected.to_not create_file(token_file) }
+          it { is_expected.to_not create_pki__copy("ds389_#{title}") }
+          it { is_expected.to_not create_exec("Validate #{title} p12") }
+          it { is_expected.to_not create_exec("Build #{title} p12") }
+          it { is_expected.to_not create_exec("Import #{title} p12") }
+          it { is_expected.to_not create_exec("Import #{title} CA") }
+          it { is_expected.to_not create_ds389__instance__dn__add("RSA DN for #{title}") }
+          it { is_expected.to_not create_ds389__instance__attr__set("Configure PKI for #{title}") }
         end
       end
     end
