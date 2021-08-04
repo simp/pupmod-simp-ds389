f15584a2d5811483d8f85276e6b16577d6b987e8
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-08-04 15:02:57 -0400

    (SIMP-10410) Reduce journal noise (#13)
    - Set the journald LogLevelMax to warning for instances to reduce unnecessary
      log noise
    
    SIMP-10410 #close

--- manifests/instance/service.pp
+++ manifests/instance/service.pp
@@ -6,7 +6,7 @@ define ds389::instance::service (
   Enum['stopped','running'] $ensure     = simplib::dlookup('ds389::instance::service', 'ensure', $name, { 'default_value' => 'running'}),
   Boolean                   $enable     = simplib::dlookup('ds389::instance::service', 'enable', $name, { 'default_value' => true}),
   Boolean                   $hasrestart = simplib::dlookup('ds389::instance::service', 'hasrestart', $name, { 'default_value' => true})
-) {
+){
   assert_private()
 
   $_instance_name = split($title, /^(dirsrv@)?slapd-/)[-1]
@@ -14,11 +14,20 @@ define ds389::instance::service (
   # Ensure that services start at boot time
   ensure_resource('service', 'dirsrv.target', { enable => true })
 
-  ensure_resource('service', "dirsrv@${_instance_name}",
-    {
-      ensure     => $ensure,
-      enable     => $enable,
-      hasrestart => $hasrestart
-    }
-  )
+  $_loglevel_override = @(OVERRIDE)
+    [Service]
+    LogLevelMax=warning
+    | OVERRIDE
+
+  ensure_resource('systemd::dropin_file', "00_dirsrv_${_instance_name}_loglevel.conf", {
+    unit    => "dirsrv@${_instance_name}.service",
+    content => $_loglevel_override
+  })
+
+  ensure_resource('service', "dirsrv@${_instance_name}", {
+    ensure     => $ensure,
+    enable     => $enable,
+    hasrestart => $hasrestart,
+    require    => Systemd::Dropin_file["00_dirsrv_${_instance_name}_loglevel.conf"]
+  })
 }
